9bd22a4ff03162a9bbea12729e089a6ccc9f5733
cfue <curtis.ch.chat@gmail.com>
2020-02-13 21:35:37 -0700

    showing forms to users

---
+++ src/components/SearchBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <div className="">
+        <form action="">
+          <input type="text" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
